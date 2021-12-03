d5fb28bc1736f179f695ada0b93f3931cb10d1b9
Sean Erle Johnson <merehap@protonmail.com>
2021-12-02 21:28:28 -0800

    Add (previously missing) clock.rs .

---
+++ src/ppu/clock.rs
@@ -0,0 +1,44 @@
+pub struct Clock {
+    frame: u64,
+    scanline: u16,
+    cycle: u16,
+}
+
+impl Clock {
+    pub fn new() -> Clock {
+        Clock {
+            frame: 0,
+            scanline: 0,
+            cycle: 0,
+        }
+    }
+
+    pub fn frame(&self) -> u64 {
+        self.frame
+    }
+
+    pub fn scanline(&self) -> u16 {
+        self.scanline
+    }
+
+    pub fn cycle(&self) -> u16 {
+        self.cycle
+    }
+
+    pub fn tick(&mut self) {
+        match (self.scanline, self.cycle) {
+            (261, 340) => {
+                self.frame += 1;
+                self.scanline = 0;
+                self.cycle = 0;
+            },
+            (_, 340) => {
+                self.scanline += 1;
+                self.cycle = 0;
+            },
+            _ => {
+                self.cycle += 1;
+            },
+        }
+    }
+}
