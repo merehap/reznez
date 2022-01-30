b5cb7e7d52065012f4062b1ad3d7cc9971c08092
Sean Erle Johnson <merehap@protonmail.com>
2022-01-30 09:30:30 -0800

    Improve performance of MappedArray::new().

--- src/util/mapped_array.rs
+++ src/util/mapped_array.rs
@@ -2,12 +2,18 @@ use std::ops::{Index, IndexMut};
 
 // One KibiByte.
 const CHUNK_LEN: usize = 0x400;
+const DUMMY_CHUNK: &'static [u8; CHUNK_LEN] = &[0; CHUNK_LEN];
 
 pub struct MappedArray<'a, const CHUNK_COUNT: usize>(
     [&'a [u8; CHUNK_LEN]; CHUNK_COUNT]
 );
 
 impl <'a, const CHUNK_COUNT: usize> MappedArray<'a, CHUNK_COUNT> {
+    // PERFORMANCE: Array allocation here is a bottleneck.
+    // If there are no mappers that use under 8KiB PRG bank sizes,
+    // then it probably makes sense to make CHUNK_LEN a const generic.
+    // Some perf can be gained once consts can be used in macros:
+    // https://github.com/rust-lang/rust/issues/52393
     pub fn new<const LEN: usize>(
         bytes: &'a [u8; LEN],
     ) -> MappedArray<'a, CHUNK_COUNT> {
@@ -16,10 +22,13 @@ impl <'a, const CHUNK_COUNT: usize> MappedArray<'a, CHUNK_COUNT> {
             LEN, CHUNK_COUNT, CHUNK_LEN,
         );
 
-        println!("LEN: {} ({}), CHUNK_COUNT: {}", LEN, bytes.len(), CHUNK_COUNT);
-        let chunks: Vec<&[u8; CHUNK_LEN]> = bytes.array_chunks::<CHUNK_LEN>().collect();
-        println!("ACTUAL CHUNK COUNT: {}. LEN: {}", chunks.len(), chunks[0].len());
-        MappedArray(chunks.try_into().unwrap())
+        let mut chunks_iter = bytes.array_chunks::<CHUNK_LEN>();
+        let mut chunks = [DUMMY_CHUNK; CHUNK_COUNT];
+        for i in 0..chunks.len() {
+            chunks[i] = chunks_iter.next().unwrap();
+        }
+
+        MappedArray(chunks)
     }
 }
 
