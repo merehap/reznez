63aa33907ba3aca7d5546209b87aa5c6d54867f6
Sean Erle Johnson <merehap@protonmail.com>
2022-02-26 19:17:27 -0800

    Change PpuAddress from a tuple struct to a struct.

--- src/memory/ppu/ppu_address.rs
+++ src/memory/ppu/ppu_address.rs
@@ -17,32 +17,38 @@ const NAME_TABLE_MASK: u16 = VERTICAL_NAME_TABLE_MASK | HORIZONTAL_NAME_TABLE_MA
 const FINE_X_MASK: u8 = 0b0000_0111;
 
 #[derive(Eq, PartialOrd, Ord, Clone, Copy, Debug)]
-pub struct PpuAddress(u16, FineXScroll);
+pub struct PpuAddress {
+    address: u16,
+    fine_x_scroll: FineXScroll,
+}
 
 impl PpuAddress {
     pub const fn from_u16(value: u16) -> PpuAddress {
-        PpuAddress(value & 0x3FFF, FineXScroll(0))
+        PpuAddress {
+            address: value & 0x3FFF,
+            fine_x_scroll: FineXScroll(0),
+        }
     }
 
     pub fn advance(&mut self, offset: u16) {
-        self.0 = self.0.wrapping_add(offset) & 0x3FFF;
+        self.address = self.address.wrapping_add(offset) & 0x3FFF;
     }
 
     pub fn subtract(&mut self, offset: u16) {
-        self.0 = self.0.wrapping_sub(offset) & 0x3FFF;
+        self.address = self.address.wrapping_sub(offset) & 0x3FFF;
     }
 
     pub fn increment_coarse_x_scroll(&mut self) {
-        if self.0 & COARSE_X_MASK == COARSE_X_MASK {
-            self.0 ^= HORIZONTAL_NAME_TABLE_MASK;
-            self.0 &= !COARSE_X_MASK;
+        if self.address & COARSE_X_MASK == COARSE_X_MASK {
+            self.address ^= HORIZONTAL_NAME_TABLE_MASK;
+            self.address &= !COARSE_X_MASK;
         } else {
-            self.0 += 1;
+            self.address += 1;
         }
     }
 
     pub fn name_table_number(self) -> NameTableNumber {
-        NameTableNumber::from_last_two_bits((self.0 >> 10) as u8)
+        NameTableNumber::from_last_two_bits((self.address >> 10) as u8)
     }
 
     /*
@@ -51,8 +57,8 @@ impl PpuAddress {
      * ----67----------  $CTRL
      */
     pub fn x_scroll(self) -> u8 {
-        let coarse_x = ((self.0 & COARSE_X_MASK) as u8) << 3;
-        let fine_x = self.1.0;
+        let coarse_x = ((self.address & COARSE_X_MASK) as u8) << 3;
+        let fine_x = self.fine_x_scroll.0;
         coarse_x | fine_x
     }
 
@@ -62,41 +68,41 @@ impl PpuAddress {
      *  ---67----------  $CTRL
      */
     pub fn y_scroll(self) -> u8 {
-        let coarse_y = (self.0 & COARSE_Y_MASK) >> 2;
-        let fine_y = (self.0 & FINE_Y_MASK) >> 12;
+        let coarse_y = (self.address & COARSE_Y_MASK) >> 2;
+        let fine_y = (self.address & FINE_Y_MASK) >> 12;
         (coarse_y | fine_y) as u8
     }
 
     pub fn set_name_table_number(&mut self, value: u8) {
-        self.0 &= !NAME_TABLE_MASK;
-        self.0 |= (value as u16 & 0b0000_0011) << 10;
+        self.address &= !NAME_TABLE_MASK;
+        self.address |= (value as u16 & 0b0000_0011) << 10;
     }
 
     pub fn set_x_scroll(&mut self, value: u8) {
-        self.1 = FineXScroll(value & FINE_X_MASK);
+        self.fine_x_scroll = FineXScroll(value & FINE_X_MASK);
 
-        self.0 &= !COARSE_X_MASK;
-        self.0 |= (value as u16) >> 3
+        self.address &= !COARSE_X_MASK;
+        self.address |= (value as u16) >> 3
     }
 
     pub fn set_y_scroll(&mut self, value: u8) {
-        self.0 &= !Y_MASK;
-        self.0 |= (value as u16 & 0b1111_1000) << 2;
-        self.0 |= (value as u16 & 0b0000_0111) << 12;
+        self.address &= !Y_MASK;
+        self.address |= (value as u16 & 0b1111_1000) << 2;
+        self.address |= (value as u16 & 0b0000_0111) << 12;
     }
 
     pub fn set_high_byte(&mut self, value: u8) {
-        self.0 &= !HIGH_BYTE_MASK;
-        self.0 |= (value as u16 & 0b0011_1111) << 8;
+        self.address &= !HIGH_BYTE_MASK;
+        self.address |= (value as u16 & 0b0011_1111) << 8;
     }
 
     pub fn set_low_byte(&mut self, value: u8) {
-        self.0 &= !LOW_BYTE_MASK;
-        self.0 |= value as u16;
+        self.address &= !LOW_BYTE_MASK;
+        self.address |= value as u16;
     }
 
     pub fn to_u16(self) -> u16 {
-        self.0 & 0x3FFF
+        self.address & 0x3FFF
     }
 
     pub fn to_usize(self) -> usize {
@@ -112,7 +118,7 @@ impl PartialEq for PpuAddress {
 
 impl fmt::Display for PpuAddress {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "${:04X}", self.0)
+        write!(f, "${:04X}", self.address)
     }
 }
 
@@ -127,7 +133,7 @@ mod tests {
     fn reduce_bit_1_from_high_byte() {
         let mut address = PpuAddress::from_u16(0);
         address.set_high_byte(0b1111_1111);
-        assert_eq!(address.0, 0b0011_1111_0000_0000);
+        assert_eq!(address.address, 0b0011_1111_0000_0000);
         assert_eq!(address.to_u16(), 0b0011_1111_0000_0000);
     }
 
@@ -135,7 +141,7 @@ mod tests {
     fn reduce_bit_1_from_y_scroll() {
         let mut address = PpuAddress::from_u16(0);
         address.set_y_scroll(0b1111_1111);
-        assert_eq!(address.0, 0b0111_0011_1110_0000);
+        assert_eq!(address.address, 0b0111_0011_1110_0000);
         assert_eq!(address.to_u16(), 0b0011_0011_1110_0000);
     }
 
@@ -143,7 +149,7 @@ mod tests {
     fn wrap_advance() {
         let mut address = PpuAddress::from_u16(0x3FFF);
         address.advance(1);
-        assert_eq!(address.0, 0x0000);
+        assert_eq!(address.address, 0x0000);
         assert_eq!(address.to_u16(), 0x0000);
     }
 
