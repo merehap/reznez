684df5a19386347ff62915dae1c3d52246da498a
Sean Erle Johnson <merehap@protonmail.com>
2022-11-25 20:18:43 -0800

    Remove redundant bank_size member variable.

--- src/memory/cpu/prg_rom.rs
+++ src/memory/cpu/prg_rom.rs
@@ -5,7 +5,6 @@ const MINIMUM_BANK_SIZE: usize = 8 * 0x400;
 
 pub struct PrgRom {
     prg_rom: Vec<u8>,
-    bank_size: usize,
     selected_bank_indexes: Vec<u8>,
 }
 
@@ -13,18 +12,16 @@ impl PrgRom {
     pub fn single_bank(bank: Box<[u8; PRG_ROM_SIZE]>) -> PrgRom {
         // Only a single bank and only a single index, which points at it.
         let prg_rom = bank.to_vec();
-        let bank_size = PRG_ROM_SIZE;
         let selected_bank_indexes = vec![0];
-        PrgRom { prg_rom, bank_size, selected_bank_indexes }
+        PrgRom { prg_rom, selected_bank_indexes }
     }
 
     pub fn single_bank_mirrored(bank: Box<[u8; PRG_ROM_SIZE / 2]>) -> PrgRom {
         // Only a single bank that is half the size of indexable PRG ROM,
         // so two indexes are necessary, both which point to the only bank.
         let prg_rom = bank.to_vec();
-        let bank_size = PRG_ROM_SIZE / 2;
         let selected_bank_indexes = vec![0, 0];
-        PrgRom { prg_rom, bank_size, selected_bank_indexes }
+        PrgRom { prg_rom, selected_bank_indexes }
     }
 
     pub fn multiple_banks(
@@ -41,8 +38,7 @@ impl PrgRom {
         assert_eq!(raw_bank_bytes.len() % bank_count, 0);
         assert_eq!(raw_bank_bytes.len() % MINIMUM_BANK_SIZE, 0);
 
-        let bank_size = raw_bank_bytes.len() / bank_count;
-        PrgRom { prg_rom: raw_bank_bytes, bank_size, selected_bank_indexes }
+        PrgRom { prg_rom: raw_bank_bytes, selected_bank_indexes }
     }
 
     pub fn selected_bank_indexes(&self) -> &[u8] {
@@ -67,9 +63,13 @@ impl PrgRom {
     fn convert_to_internal_index(&self, external_index: usize) -> usize {
         assert!(external_index < PRG_ROM_SIZE);
 
-        let placement  = external_index / self.bank_size;
+        let placement  = external_index / self.bank_size();
         let bank_index = usize::from(self.selected_bank_indexes[placement]);
-        let byte_index = external_index % self.bank_size;
-        self.bank_size * bank_index + byte_index
+        let byte_index = external_index % self.bank_size();
+        self.bank_size() * bank_index + byte_index
+    }
+
+    fn bank_size(&self) -> usize {
+        PRG_ROM_SIZE / self.selected_bank_indexes.len()
     }
 }
