8c20edb7cfb725942a4a393ba94756887688fbe9
Sean Erle Johnson <merehap@protonmail.com>
2023-02-27 16:49:41 -0800

    Move Mapper 2 to InitialLayout.

--- src/memory/mappers/mapper2.rs
+++ src/memory/mappers/mapper2.rs
@@ -1,19 +1,27 @@
 use crate::memory::mapper::*;
 
-lazy_static! {
-    static ref PRG_LAYOUT: PrgLayout = PrgLayout::builder()
-        .max_bank_count(256)
-        .bank_size(16 * KIBIBYTE)
-        .window(0x6000, 0x7FFF,  8 * KIBIBYTE, PrgType::Empty)
-        .window(0x8000, 0xBFFF, 16 * KIBIBYTE, PrgType::Banked(Rom, BankIndex::Register(P0)))
-        .window(0xC000, 0xFFFF, 16 * KIBIBYTE, PrgType::Banked(Rom, BankIndex::LAST))
-        .build();
-    static ref CHR_LAYOUT: ChrLayout = ChrLayout::builder()
-        .max_bank_count(1)
-        .bank_size(8 * KIBIBYTE)
-        .window(0x0000, 0x1FFF, 8 * KIBIBYTE, ChrType(Rom, BankIndex::FIRST))
-        .build();
-}
+const INITIAL_LAYOUT: InitialLayout = InitialLayout {
+    // TODO: Figure out how to fix this for mirrored memory, if necessary
+    prg_max_bank_count: 256,
+    prg_bank_size: 16 * KIBIBYTE,
+    prg_windows_by_board: &[(Board::Any, PRG_WINDOWS)],
+
+    chr_max_bank_count: 1,
+    chr_bank_size: 8 * KIBIBYTE,
+    chr_windows: CHR_WINDOWS,
+
+    name_table_mirroring_source: NameTableMirroringSource::Cartridge,
+};
+
+const PRG_WINDOWS: &[PrgWindow] = &[
+    PrgWindow::new(0x6000, 0x7FFF,  8 * KIBIBYTE, PrgType::Empty),
+    PrgWindow::new(0x8000, 0xBFFF, 16 * KIBIBYTE, PrgType::Banked(Rom, BankIndex::Register(P0))),
+    PrgWindow::new(0xC000, 0xFFFF, 16 * KIBIBYTE, PrgType::Banked(Rom, BankIndex::LAST)),
+];
+
+const CHR_WINDOWS: &[ChrWindow] = &[
+    ChrWindow::new(0x0000, 0x1FFF, 8 * KIBIBYTE, ChrType(Rom, BankIndex::FIRST)),
+];
 
 // UxROM (common usages)
 pub struct Mapper2 {
@@ -35,11 +43,7 @@ impl Mapper for Mapper2 {
 
 impl Mapper2 {
     pub fn new(cartridge: &Cartridge) -> Result<Mapper2, String> {
-        Ok(Mapper2 { params: MapperParams::new(
-            cartridge,
-            PRG_LAYOUT.clone(),
-            CHR_LAYOUT.clone(),
-            cartridge.name_table_mirroring(),
-        )})
+        let params = INITIAL_LAYOUT.make_mapper_params(cartridge, Board::Any);
+        Ok(Mapper2 { params })
     }
 }
