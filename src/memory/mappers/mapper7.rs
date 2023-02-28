02aa89da8bddee47e34ee424df93be6d64625580
Sean Erle Johnson <merehap@protonmail.com>
2023-02-27 17:05:35 -0800

    Make mapper 7 use InitialLayout.

--- src/memory/mappers/mapper7.rs
+++ src/memory/mappers/mapper7.rs
@@ -1,19 +1,25 @@
 use crate::memory::mapper::*;
 
-lazy_static! {
-    static ref PRG_LAYOUT: PrgLayout = PrgLayout::builder()
-        .max_bank_count(8)
-        .bank_size(32 * KIBIBYTE)
-        .window(0x6000, 0x7FFF,  8 * KIBIBYTE, PrgType::Empty)
-        .window(0x8000, 0xFFFF, 32 * KIBIBYTE, PrgType::Banked(Rom, BankIndex::Register(P0)))
-        .build();
-    // Only one bank, so not bank-switched.
-    static ref CHR_LAYOUT: ChrLayout = ChrLayout::builder()
-        .max_bank_count(1)
-        .bank_size(8 * KIBIBYTE)
-        .window(0x0000, 0x1FFF, 8 * KIBIBYTE, ChrType(Ram, BankIndex::FIRST))
-        .build();
-}
+const INITIAL_LAYOUT: InitialLayout = InitialLayout {
+    prg_max_bank_count: 2,
+    prg_bank_size: 16 * KIBIBYTE,
+    prg_windows_by_board: &[(Board::Any, PRG_WINDOWS)],
+
+    chr_max_bank_count: 1,
+    chr_bank_size: 8 * KIBIBYTE,
+    chr_windows: CHR_WINDOWS,
+
+    name_table_mirroring_source: NameTableMirroringSource::Cartridge,
+};
+
+const PRG_WINDOWS: &[PrgWindow] = &[
+    PrgWindow::new(0x6000, 0x7FFF,  8 * KIBIBYTE, PrgType::Empty),
+    PrgWindow::new(0x8000, 0xFFFF, 32 * KIBIBYTE, PrgType::Banked(Rom, BankIndex::Register(P0))),
+];
+
+const CHR_WINDOWS: &[ChrWindow] = &[
+    ChrWindow::new(0x0000, 0x1FFF, 8 * KIBIBYTE, ChrType(Rom, BankIndex::FIRST)),
+];
 
 // AxROM
 pub struct Mapper7 {
@@ -42,11 +48,7 @@ impl Mapper for Mapper7 {
 
 impl Mapper7 {
     pub fn new(cartridge: &Cartridge) -> Result<Mapper7, String> {
-        Ok(Mapper7 { params: MapperParams::new(
-            cartridge,
-            PRG_LAYOUT.clone(),
-            CHR_LAYOUT.clone(),
-            NameTableMirroring::OneScreenLeftBank,
-        )})
+        let params = INITIAL_LAYOUT.make_mapper_params(cartridge, Board::Any);
+        Ok(Mapper7 { params })
     }
 }
