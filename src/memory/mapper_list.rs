22ae56cb9bae7ad8aac4a30680060f4b2c51784f
Sean Erle Johnson <merehap@protonmail.com>
2024-03-28 20:35:16 -0700

    Split out separate mapper 4 submappers.

--- src/memory/mapper_list.rs
+++ src/memory/mapper_list.rs
@@ -12,6 +12,9 @@ pub fn lookup_mapper(cartridge: &Cartridge) -> (Box<dyn Mapper>, MapperParams) {
         (2, _) => Box::new(m::mapper002::Mapper002),
         (3, _) => Box::new(m::mapper003::Mapper003),
         (4, 0) => Box::new(m::mapper004::Mapper004::new()),
+        (4, 1) => Box::new(m::mapper004::Mapper004::new()),
+        (4, 3) => Box::new(m::mapper004::Mapper004::new()),
+        (4, 4) => Box::new(m::mapper004::Mapper004::new()),
         (5, 0) => Box::new(m::mapper005::Mapper005::new()),
 
         (7, _) => Box::new(m::mapper007::Mapper007),
