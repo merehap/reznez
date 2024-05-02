1acd5809cefb565f391ad45194135d3c1661fc37
Sean Erle Johnson <merehap@protonmail.com>
2024-05-02 15:41:49 -0700

    Factor mapper 1 shift logic into ShiftRegister

--- src/memory/mappers/mapper001_0.rs
+++ src/memory/mappers/mapper001_0.rs
@@ -24,11 +24,9 @@ const CHR_LAYOUT_TWO_SMALL: ChrLayout = ChrLayout::new(&[
     ChrWindow::new(0x1000, 0x1FFF, 4 * KIBIBYTE, ChrBank::Switchable(Ram, C1)),
 ]);
 
-const EMPTY_SHIFT_REGISTER: u8 = 0b0001_0000;
-
 // SxROM (MMC1)
 pub struct Mapper001_0 {
-    shift: u8,
+    shift_register: ShiftRegister,
 }
 
 impl Mapper for Mapper001_0 {
@@ -51,52 +49,38 @@ impl Mapper for Mapper001_0 {
             return;
         }
 
-        if value & 0b1000_0000 != 0 {
-            self.shift = EMPTY_SHIFT_REGISTER;
-            params.set_prg_layout(PRG_LAYOUT_FIXED_LAST);
-            return;
-        }
-
-        let is_last_shift = self.shift & 1 == 1;
-
-        self.shift >>= 1;
-        self.shift |= (value & 1) << 4;
-
-        if !is_last_shift {
-            return;
-        }
-
-        let shift = self.shift;
-        match address.to_raw() {
-            0x0000..=0x401F => unreachable!(),
-            0x4020..=0x5FFF => { /* Do nothing. */ }
-            0x6000..=0x7FFF => unreachable!(),
-            0x8000..=0x9FFF => {
-                params.set_prg_layout(Mapper001_0::next_prg_windows(shift));
-                params.set_chr_layout(Mapper001_0::next_chr_windows(shift));
-                params.set_name_table_mirroring(Mapper001_0::next_mirroring(shift));
-            }
-            // FIXME: Handle cases for special boards.
-            0xA000..=0xBFFF => params.set_bank_index_register(C0, shift),
-            // FIXME: Handle cases for special boards.
-            0xC000..=0xDFFF => params.set_bank_index_register(C1, shift),
-            0xE000..=0xFFFF => {
-                params.set_bank_index_register(P0, shift & 0b0_1111);
-                if shift & 0b1_0000 == 0 {
-                    params.enable_work_ram(0x6000);
-                } else {
-                    params.disable_work_ram(0x6000);
+        match self.shift_register.shift(value) {
+            ShiftStatus::Clear => params.set_prg_layout(PRG_LAYOUT_FIXED_LAST),
+            ShiftStatus::Continue => { /* Do nothing additional. */ }
+            ShiftStatus::Done { finished_value } => match address.to_raw() {
+                0x0000..=0x401F => unreachable!(),
+                0x4020..=0x5FFF => { /* Do nothing. */ }
+                0x6000..=0x7FFF => unreachable!(),
+                0x8000..=0x9FFF => {
+                    params.set_prg_layout(Mapper001_0::next_prg_windows(finished_value));
+                    params.set_chr_layout(Mapper001_0::next_chr_windows(finished_value));
+                    params.set_name_table_mirroring(Mapper001_0::next_mirroring(finished_value));
+                }
+                // FIXME: Handle cases for special boards.
+                0xA000..=0xBFFF => params.set_bank_index_register(C0, finished_value),
+                // FIXME: Handle cases for special boards.
+                0xC000..=0xDFFF => params.set_bank_index_register(C1, finished_value),
+                0xE000..=0xFFFF => {
+                    params.set_bank_index_register(P0, finished_value & 0b0_1111);
+                    if finished_value & 0b1_0000 == 0 {
+                        params.enable_work_ram(0x6000);
+                    } else {
+                        params.disable_work_ram(0x6000);
+                    }
                 }
             }
         }
-
-        self.shift = EMPTY_SHIFT_REGISTER;
     }
 }
 
 impl Mapper001_0 {
     pub fn new() -> Self {
-        Self { shift: EMPTY_SHIFT_REGISTER }
+        Self { shift_register: ShiftRegister::new() }
     }
 
     fn next_prg_windows(value: u8) -> PrgLayout {
@@ -125,4 +109,43 @@ impl Mapper001_0 {
             _ => unreachable!(),
         }
     }
+}
+
+const EMPTY_SHIFT_REGISTER: u8 = 0b0001_0000;
+
+pub struct ShiftRegister {
+    value: u8,
+}
+
+impl ShiftRegister {
+    pub fn new() -> Self {
+        Self { value: EMPTY_SHIFT_REGISTER }
+    }
+
+    pub fn shift(&mut self, write_value: u8) -> ShiftStatus {
+        if write_value & 0b1000_0000 != 0 {
+            self.value = EMPTY_SHIFT_REGISTER;
+            return ShiftStatus::Clear;
+        }
+
+        let is_last_shift = self.value & 1 == 1;
+        self.value >>= 1;
+        // Copy the last bit from write_value to the front of self.value.
+        self.value |= (write_value & 1) << 4;
+
+        if !is_last_shift {
+            return ShiftStatus::Continue;
+        }
+
+        let finished_value = self.value;
+        self.value = EMPTY_SHIFT_REGISTER;
+        ShiftStatus::Done { finished_value }
+    }
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+pub enum ShiftStatus {
+    Clear,
+    Continue,
+    Done { finished_value: u8 },
 }
