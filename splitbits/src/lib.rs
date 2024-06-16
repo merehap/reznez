e36debc4bacf04b3fe61219829a11a96ffbe89cc
Sean Erle Johnson <merehap@protonmail.com>
2024-06-16 10:01:11 -0700

    splitbits: Refactor code into Template type

--- splitbits/src/lib.rs
+++ splitbits/src/lib.rs
@@ -19,6 +19,7 @@ use syn::punctuated::Punctuated;
 // * Allow non-const variable templates (as a separate macro?).
 // * Better error messages.
 // * Remove itertools dependency.
+// * Allow non-standard template lengths.
 #[proc_macro]
 pub fn splitbits(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     splitbits_base(input, Base::Binary, Precision::Standard)
@@ -100,26 +101,20 @@ pub fn onehexfield_ux(input: proc_macro::TokenStream) -> proc_macro::TokenStream
 }
 
 fn splitbits_base(input: proc_macro::TokenStream, base: Base, precision: Precision) -> proc_macro::TokenStream {
-    let (value, template) = parse_input(input.into());
-    let (template, input_type) = apply_base(&template, base);
-    let fields = fields(input_type, value, template.clone(), precision);
+    let (value, template) = parse_input(input.into(), base, precision);
+    let fields = template.extract_fields(&value);
 
-    let struct_name_suffix: String = template.iter()
-        // Underscores work in struct names, periods do not.
-        .map(|&c| if c == '.' { '_' } else { c })
-        .collect();
-    let struct_ident = format_ident!("{}", format!("Fields·{}", struct_name_suffix));
-
+    let struct_name = template.to_struct_name();
     let names: Vec<_> = fields.iter().map(|field| field.name()).collect();
     let types: Vec<_> = fields.iter().map(|field| field.t()).collect();
     let values: Vec<TokenStream> = fields.iter().map(|field| field.value.clone()).collect();
     let result = quote! {
         {
-            struct #struct_ident {
+            struct #struct_name {
                 #(#names: #types,)*
             }
 
-            #struct_ident {
+            #struct_name {
                 #(#names: #values,)*
             }
         }
@@ -129,9 +124,8 @@ fn splitbits_base(input: proc_macro::TokenStream, base: Base, precision: Precisi
 }
 
 fn splitbits_tuple_base(input: proc_macro::TokenStream, base: Base, precision: Precision) -> proc_macro::TokenStream {
-    let (value, template) = parse_input(input.into());
-    let (template, input_type) = apply_base(&template, base);
-    let fields = fields(input_type, value, template.clone(), precision);
+    let (value, template) = parse_input(input.into(), base, precision);
+    let fields = template.extract_fields(&value);
     let values: Vec<TokenStream> = fields.iter().map(|field| field.value.clone()).collect();
 
     let result = quote! {
@@ -142,9 +136,8 @@ fn splitbits_tuple_base(input: proc_macro::TokenStream, base: Base, precision: P
 }
 
 fn splitbits_tuple_into_base(input: proc_macro::TokenStream, base: Base, precision: Precision) -> proc_macro::TokenStream {
-    let (value, template) = parse_input(input.into());
-    let (template, input_type) = apply_base(&template, base);
-    let fields = fields(input_type, value, template.clone(), precision);
+    let (value, template) = parse_input(input.into(), base, precision);
+    let fields = template.extract_fields(&value);
     let values: Vec<TokenStream> = fields.iter().map(|field| field.value.clone()).collect();
 
     let result = quote! {
@@ -155,25 +148,13 @@ fn splitbits_tuple_into_base(input: proc_macro::TokenStream, base: Base, precisi
 }
 
 fn onefield_base(input: proc_macro::TokenStream, base: Base, precision: Precision) -> proc_macro::TokenStream {
-    let (value, template) = parse_input(input.into());
-    let (template, input_type) = apply_base(&template, base);
-    let fields = fields(input_type, value, template.clone(), precision);
+    let (value, template) = parse_input(input.into(), base, precision);
+    let fields = template.extract_fields(&value);
     assert_eq!(fields.len(), 1);
     fields[0].value.clone().into()
 }
 
-fn apply_base(template: &[char], base: Base) -> (Vec<char>, Type) {
-    let input_type = Type::from_template(template, base);
-    // Each template char needs to be repeated if we aren't working in base 2.
-    let bit_template = template.iter()
-        .cloned()
-        .flat_map(|n| std::iter::repeat(n).take(base.bits_per_digit()))
-        .collect();
-
-    (bit_template, input_type)
-}
-
-fn parse_input(item: TokenStream) -> (Expr, Vec<char>) {
+fn parse_input(item: TokenStream, base: Base, precision: Precision) -> (Expr, Template) {
     let parts: Punctuated::<Expr, Token![,]> = Parser::parse2(
         Punctuated::<Expr, Token![,]>::parse_terminated,
         item.clone().into(),
@@ -182,30 +163,89 @@ fn parse_input(item: TokenStream) -> (Expr, Vec<char>) {
     assert_eq!(parts.len(), 2);
 
     let value = parts[0].clone();
-    let Expr::Lit(template) = parts[1].clone() else { panic!() };
-    let Lit::Str(template) = template.lit else { panic!() };
-    let template: String = template.value();
-
-    let template: Vec<char> = template.chars()
-        // Spaces are only for human-readability.
-        .filter(|&c| c != ' ')
-        .collect();
-
+    let template = Template::from_expr(&parts[1], base, precision);
     (value, template)
 }
 
-fn fields(input_type: Type, input: Expr, template: Vec<char>, precision: Precision) -> Vec<Field> {
-    template.clone()
-        .iter()
-        .unique()
-        // Periods aren't names, they are placeholders for ignored bits.
-        .filter(|&&n| n != '.')
-        .map(|&name| {
-            assert!(name.is_ascii_lowercase());
-            Field::new(name, input_type, &input, &template, precision)
-        })
-        .collect()
+struct Template {
+    input_type: Type,
+    precision: Precision,
+    names: Vec<Name>,
+    locations_by_name: Vec<(Name, Vec<Location>)>,
 }
+
+impl Template {
+    fn from_expr(template: &Expr, base: Base, precision: Precision) -> Template {
+        let Expr::Lit(template) = template.clone() else { panic!() };
+        let Lit::Str(template) = template.lit else { panic!() };
+        let template: Vec<char> = template.value().chars()
+            // Spaces are only for human-readability.
+            .filter(|&c| c != ' ')
+            .collect();
+
+        let names: Vec<Name> = template.clone()
+            .iter()
+            // Periods aren't names, they are placeholders for ignored bits.
+            .filter(|&&n| n != '.')
+            .map(|&name| { assert!(name.is_ascii_lowercase()); name })
+            .unique()
+            .collect();
+
+        // Each template char needs to be repeated if we aren't working in base 2.
+        let template: Vec<_> = template.iter()
+            .cloned()
+            .flat_map(|n| std::iter::repeat(n).take(base.bits_per_digit()))
+            .collect();
+
+        let input_type = match template.len() {
+            8 => Type(8),
+            16 => Type(16),
+            32 => Type(32),
+            64 => Type(64),
+            128 => Type(128),
+            len => panic!("Template length must be 8, 16, 32, 64, or 128, but was {len}."),
+        };
+
+        let mut locations_by_name: Vec<(Name, Vec<Location>)> = Vec::new();
+        for &name in &names {
+            let locations: Vec<Location> = template.iter()
+                .rev()
+                .enumerate()
+                .chunk_by(|(_, &n)| n)
+                .into_iter()
+                .filter_map(|(n, p)| if n == name { Some(p) } else { None })
+                .map(|p| {
+                    let p: Vec<_> = p.collect();
+                    (p.len() as u32, p[0].0 as u32)
+                })
+                .collect();
+            locations_by_name.push((name, locations));
+        }
+
+        Template { input_type, precision, names, locations_by_name }
+    }
+
+    fn names(&self) -> &[char] {
+        &self.names
+    }
+
+    fn extract_fields(&self, input: &Expr) -> Vec<Field> {
+        self.locations_by_name.iter()
+            .map(|(name, locations)| Field::new(*name, self.input_type, input, self.precision, &locations))
+            .collect()
+    }
+
+    fn to_struct_name(&self) -> Ident {
+        let struct_name_suffix: String = self.names().iter()
+            // Underscores work in struct names, periods do not.
+            .map(|&c| if c == '.' { '_' } else { c })
+            .collect();
+        format_ident!("{}", format!("Fields·{}", struct_name_suffix))
+    }
+}
+
+type Name = char;
+type Location = (u32, u32);
 
 #[derive(Clone)]
 struct Field {
@@ -215,19 +255,7 @@ struct Field {
 }
 
 impl Field {
-    fn new(name: char, input_type: Type, input: &Expr, template: &[char], precision: Precision) -> Field {
-        let locations: Vec<_> = template.iter()
-            .rev()
-            .enumerate()
-            .chunk_by(|(_, &n)| n)
-            .into_iter()
-            .filter_map(|(n, p)| if n == name { Some(p) } else { None })
-            .map(|p| {
-                let p: Vec<_> = p.collect();
-                (p.len() as u32, p[0].0 as u32)
-            })
-            .collect();
-
+    fn new(name: char, input_type: Type, input: &Expr, precision: Precision, locations: &[Location]) -> Field {
         let bit_count = locations.iter().map(|(length, _)| length).sum();
         let t = match bit_count {
             0 => panic!(),
@@ -251,7 +279,7 @@ impl Field {
         } else {
             let mut segment_offset = 0;
             let mut segments = Vec::new();
-            for (length, mask_offset) in locations {
+            for &(length, mask_offset) in locations {
                 let mut mask: u128 = 2u128.pow(length as u32) - 1;
                 mask <<= mask_offset;
 
@@ -294,17 +322,6 @@ struct Type(u8);
 
 impl Type {
     const BOOL: Type = Type(1);
-
-    fn from_template(template: &[char], base: Base) -> Type {
-        match base.bits_per_digit() * template.len() {
-            8 => Type(8),
-            16 => Type(16),
-            32 => Type(32),
-            64 => Type(64),
-            128 => Type(128),
-            len => panic!("Template length must be 8, 16, 32, 64, or 128, but was {len}."),
-        }
-    }
 }
 
 impl From<u8> for Type {
