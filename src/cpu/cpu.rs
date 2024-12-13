4155de76ce91502cd6c9be736f9b3cb33db0425b
Sean Erle Johnson <merehap@protonmail.com>
2024-12-13 14:35:08 -0800

    Move interrupt sequence start into CycleAction

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -212,7 +212,6 @@ impl Cpu {
             }
         }
 
-        let mut execute_cycle_actions = true;
         let mut dma_address = None;
         if step.is_read() && let Some(address) = memory.take_dmc_dma_pending_address() {
             info!(target: "cpuflowcontrol", "Reading DMC DMA byte at {address}.");
@@ -224,40 +223,13 @@ impl Cpu {
             // TODO: Strip out unused CycleActions.
             info!(target: "cpuflowcontrol", "Starting OAM DMA transfer at {}.",
                 self.oam_dma_port.current_address());
-            execute_cycle_actions = false;
             self.mode_state.oam_dma_pending();
-        } else if step.has_start_new_instruction() {
-            if self.reset_status == ResetStatus::Ready {
-                info!(target: "cpuflowcontrol", "Starting system reset");
-                self.reset_status = ResetStatus::Active;
-                // Reset has BRK's code point (0x00). TODO: Set the data bus to 0x00?
-                self.next_op_code = Some((0x00, self.address_bus));
-                self.mode_state.set_next_mode(CpuMode::Reset);
-                execute_cycle_actions = false;
-            } else if self.nmi_status == NmiStatus::Ready {
-                info!(target: "cpuflowcontrol", "Starting NMI");
-                self.nmi_status = NmiStatus::Active;
-            } else if self.irq_status == IrqStatus::Ready && self.nmi_status == NmiStatus::Inactive {
-                info!(target: "cpuflowcontrol", "Starting IRQ");
-                self.irq_status = IrqStatus::Active;
-            }
-        }
-
-        if execute_cycle_actions {
+        } else {
             for &action in step.actions() {
                 self.execute_cycle_action(memory, action, irq_pending);
             }
 
             self.suppress_program_counter_increment = false;
-
-            if step.has_start_new_instruction() && !self.suppress_next_instruction_start {
-                if self.interrupt_sequence_active() {
-                    self.current_instruction = None;
-                } else if let Some((next_op_code, _)) = self.next_op_code {
-                    self.current_instruction = Some(Instruction::from_code_point(next_op_code));
-                    self.mode_state.instruction(INSTRUCTIONS[next_op_code as usize].steps());
-                }
-            }
 
             if let Some(dma_address) = dma_address {
                 let new_sample_buffer = memory.read(dma_address).unwrap_or(self.data_bus);
@@ -283,9 +255,26 @@ impl Cpu {
                     return;
                 }
 
-                if self.nmi_status == NmiStatus::Active || self.irq_status == IrqStatus::Active {
+                self.current_instruction = None;
+                if self.reset_status == ResetStatus::Ready {
+                    info!(target: "cpuflowcontrol", "Starting system reset");
+                    self.reset_status = ResetStatus::Active;
+                    self.data_bus = 0x00;
+                    self.next_op_code = Some((0x00, self.address_bus));
+                    self.mode_state.set_next_mode(CpuMode::Reset);
+                } else if self.nmi_status == NmiStatus::Ready {
+                    info!(target: "cpuflowcontrol", "Starting NMI");
+                    self.nmi_status = NmiStatus::Active;
                     self.data_bus = 0x00;
                     self.mode_state.set_next_mode(CpuMode::InterruptSequence);
+                } else if self.irq_status == IrqStatus::Ready && self.nmi_status == NmiStatus::Inactive {
+                    info!(target: "cpuflowcontrol", "Starting IRQ");
+                    self.irq_status = IrqStatus::Active;
+                    self.data_bus = 0x00;
+                    self.mode_state.set_next_mode(CpuMode::InterruptSequence);
+                } else {
+                    self.current_instruction = Some(Instruction::from_code_point(self.data_bus));
+                    self.mode_state.instruction(INSTRUCTIONS[self.data_bus as usize].steps());
                 }
 
                 self.next_op_code = Some((self.data_bus, self.address_bus));
