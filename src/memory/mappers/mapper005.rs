4f8fb644169a0376c288dd6a746ea4a776a4b31d
Sean Erle Johnson <merehap@protonmail.com>
2025-02-18 09:02:00 -0800

    Remove ScanlineDetector state machine
    Slight improvement to code clarity.

--- src/memory/mappers/mapper005.rs
+++ src/memory/mappers/mapper005.rs
@@ -448,7 +448,7 @@ impl FrameState {
     fn new() -> Self {
         Self {
             stage: FrameStage::OutOfFrame,
-            lock_state: ScanlineDetector::ScanlineDetected,
+            lock_state: ScanlineDetector::new(),
             ppu_is_reading: false,
             scanline: 0,
             // A target of 0 means IRQs are disabled (unless one was already pending).
@@ -544,49 +544,48 @@ enum FrameStage {
 }
 
 // Determines when a new scanline is detected, if rendering is enabled.
-// Indicates that the "in frame" state may occur when ScanlineDetected.
-#[derive(PartialEq, Eq, Debug)]
-enum ScanlineDetector {
-    // The last PPU read was NOT from a name table.
-    DidNotMatch,
-    // The last PPU read was from a name table entry.
-    FirstMatch(PpuAddress),
-    // The last TWO PPU reads were from the same name table entry. 
-    SecondMatch(PpuAddress),
-    // There were THREE consecutive PPU reads of the same name table entry, and no subsequent reads
-    // outside of the name table memory range have occurred.
-    ScanlineDetected,
+// Indicates that the "in frame" state may occur when scanline_detected() is true.
+pub struct ScanlineDetector {
+    // How many PPU reads in a row have had the same in-range address.
+    match_count: u8,
+    // The previous PPU address read.
+    prev_addr: PpuAddress,
 }
 
 impl ScanlineDetector {
-    fn scanline_detected(&self) -> bool {
-        *self == ScanlineDetector::ScanlineDetected
+    pub fn new() -> Self {
+        Self {
+            // MMC5 powers up with the assumption that a scanline is detected:
+            // https://www.nesdev.org/wiki/File:Mmc5_in_frame_status_bit.png
+            match_count: 3,
+            prev_addr: PpuAddress::ZERO,
+        }
     }
 
-    fn step(&mut self, addr: PpuAddress) -> bool {
-        use ScanlineDetector::*;
+    pub fn scanline_detected(&self) -> bool {
+        self.match_count >= 3
+    }
 
-        let mut new_scanline_detected = false;
+    pub fn step(&mut self, addr: PpuAddress) -> bool {
+        let prev_match_count = self.match_count;
 
         let is_in_name_table = matches!(addr.to_u16(), 0x2000..=0x2FFF);
-        if !is_in_name_table {
-            // Address out of range, go back to the beginning.
-            *self = DidNotMatch;
-            return new_scanline_detected;
+        // If match_count == 0 or a scanline has been detected, then ignore address mismatches.
+        let mismatched_addrs = matches!(self.match_count, 1 | 2) && self.prev_addr != addr;
+        if !is_in_name_table || mismatched_addrs {
+            // Address out of range or doesn't match the previous one, go back to the beginning.
+            self.match_count = 0;
+            // No scanline detected.
+            return false;
         }
 
-        *self = match *self {
-            DidNotMatch => FirstMatch(addr),
-            FirstMatch(prev_addr) if prev_addr == addr => SecondMatch(addr),
-            SecondMatch(prev_addr) if prev_addr == addr => {
-                new_scanline_detected = true;
-                ScanlineDetected
-            }
-            // Addresses didn't match, go back to the beginning.
-            FirstMatch(_) | SecondMatch(_) => DidNotMatch,
-            ScanlineDetected => ScanlineDetected,
-        };
+        if self.match_count < 3 {
+            self.match_count += 1;
+        }
 
+        self.prev_addr = addr;
+
+        let new_scanline_detected = prev_match_count == 2 && self.match_count == 3;
         new_scanline_detected
     }
 }
