64341b61b2e59cb513e04eb5630189be47755e1f
Sean Erle Johnson <merehap@protonmail.com>
2025-03-04 09:18:44 -0800

    Classify all MMC1 board types

--- src/memory/mappers/mmc1/board.rs
+++ src/memory/mappers/mmc1/board.rs
@@ -1,17 +1,38 @@
+use log::warn;
+
 use crate::cartridge::cartridge::Cartridge;
 use crate::util::unit::KIBIBYTE;
 
+#[allow(non_camel_case_types)]
+#[allow(clippy::upper_case_acronyms)]
 #[derive(PartialEq, Eq, Debug)]
 pub enum Board {
     Unknown,
-    // PRG ROM <= 256k, CHR RAM = 8k, PRG RAM = 8k
-    Snrom,
-    // PRG RAM = 16k
-    Sorom,
-    // 
-    Surom,
-    // PRG RAM = 32k
-    Sxrom
+
+    SAROM,
+    SBROM,
+    // Including Sc1rom.
+    SCROM_SL1ROM,
+    SEROM,
+    // Only the 128KiB PRGROM variants of SFROM.
+    SFROM128,
+    // Includes SF1ROM and SFEXPROM.
+    SFROM256,
+    SGROM,
+    SGROM_SMROM,
+    // Includes SHR1ROM.
+    SHROM,
+    SIROM,
+    SJROM,
+    SKROM,
+    // SLROM, SL1ROM (except 64KiB PRG), SL2ROM, SL3ROM, SLRROM.
+    // This can be broken down further if desired.
+    SLROM,
+    SNROM,
+    SOROM,
+    SUROM,
+    SXROM,
+    SZROM,
 }
 
 
@@ -20,22 +41,44 @@ impl Board {
         let prg_rom_size = cartridge.prg_rom_size() / KIBIBYTE;
         let prg_ram_size = cartridge.prg_ram_size() / KIBIBYTE;
         let chr_rom_size = cartridge.chr_rom_size() / KIBIBYTE;
-        let chr_ram_size = cartridge.chr_ram_size() / KIBIBYTE;
+        let mut chr_ram_size = cartridge.chr_ram_size() / KIBIBYTE;
+        // FIXME: Hack for ROMs that don't specify CHR sizes.
+        if chr_rom_size == 0 && chr_ram_size == 0 {
+            chr_ram_size = 8;
+        }
 
         use Board::*;
         let board = match (prg_rom_size, prg_ram_size, chr_rom_size, chr_ram_size) {
-            (128 | 256      ,  8, 8, 0) => Snrom,
-            (128 | 256      ,  8, 0, 8) => Snrom,
-            (128 | 256      , 16, 8, 0) => Sorom,
-            (128 | 256      , 16, 0, 8) => Sorom,
-            (      512      ,  8, 8, 0) => Surom,
-            (      512      ,  8, 0, 8) => Surom,
-            (128 | 256 | 512, 32, 8, 0) => Sxrom,
-            (128 | 256 | 512, 32, 0, 8) => Sxrom,
-            _ => Unknown,
+            (64             ,  8, 16 | 32 | 64, 0) => SAROM,
+            (64             ,  0, 16 | 32 | 64, 0) => SBROM,
+            (64             ,  0,          128, 0) => SCROM_SL1ROM,
+            (32             ,  0, 16 | 32 | 64, 0) => SEROM,
+            (128            ,  0, 16 | 32 | 64, 0) => SFROM128,
+            (256            ,  0, 16 | 32 | 64, 0) => SFROM256,
+            (128            ,  0,            8, 0) => SGROM,
+            (128 | 256      ,  0,            0, 8) => SGROM,
+            (256            ,  0,            8, 0) => SGROM_SMROM,
+            (32             ,  0,          128, 0) => SHROM,
+            (32             ,  8, 16 | 32 | 64, 0) => SIROM,
+            (128 | 256      ,  8, 16 | 32 | 64, 0) => SJROM,
+            (128 | 256      ,  8,          128, 0) => SKROM,
+            (128 | 256      ,  0,          128, 0) => SLROM,
+            (128 | 256      ,  8,            8, 0) => SNROM,
+            (128 | 256      ,  8,            0, 8) => SNROM,
+            (128 | 256      , 16,            8, 0) => SOROM,
+            (128 | 256      , 16,            0, 8) => SOROM,
+            (      512      ,  8,            8, 0) => SUROM,
+            (      512      ,  8,            0, 8) => SUROM,
+            (128 | 256 | 512, 32,            8, 0) => SXROM,
+            (128 | 256 | 512, 32,            0, 8) => SXROM,
+            (128 | 256      , 16, 16 | 32 | 64, 0) => SZROM,
+            _ => {
+                warn!("Unknown MMC1 board: ({prg_rom_size}KiB, {prg_ram_size}KiB, {chr_rom_size}KiB, {chr_ram_size}KiB)");
+                Unknown
+            }
         };
 
-        if matches!(board, Snrom | Sorom | Sxrom) {
+        if matches!(board, SEROM | SHROM | SNROM | SOROM | SUROM | SXROM | SZROM) {
             todo!("MMC1 {board:?} is not yet supported.");
         }
 
