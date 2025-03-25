5337ae34b2bff416db4ca8d0679bf394c1eb394f
Sean Erle Johnson <merehap@protonmail.com>
2025-03-24 20:41:04 -0700

    Add warning about mislabled mapper 152 ROMs

--- src/memory/mappers/mapper070.rs
+++ src/memory/mappers/mapper070.rs
@@ -1,7 +1,9 @@
+use log::error;
+
 use crate::memory::mapper::*;
 
 const LAYOUT: Layout = Layout::builder()
-    .prg_rom_max_size(256 * KIBIBYTE)
+    .prg_rom_max_size(128 * KIBIBYTE)
     .prg_layout(&[
         Window::new(0x6000, 0x7FFF,  8 * KIBIBYTE, Bank::EMPTY),
         Window::new(0x8000, 0xBFFF, 16 * KIBIBYTE, Bank::ROM.switchable(P0)),
@@ -25,9 +27,13 @@ impl Mapper for Mapper070 {
             0x0000..=0x401F => unreachable!(),
             0x4020..=0x7FFF => { /* Do nothing. */ },
             0x8000..=0xFFFF => {
-                let banks = splitbits!(value, "ppppcccc");
+                let banks = splitbits!(value, "mpppcccc");
                 params.set_bank_register(P0, banks.p);
                 params.set_bank_register(C0, banks.c);
+
+                if banks.m {
+                    error!("This is probably a mapper 152 game mislabeled as mapper 70. Mapper 70 doesn't support changing mirroring.");
+                }
             }
         }
     }
