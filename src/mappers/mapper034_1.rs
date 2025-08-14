895fad75fad3a5326a3ad604d0119d4804162c6b
Sean Erle Johnson <merehap@protonmail.com>
2025-08-14 08:55:11 -0700

    Temporarily remove NINA-01 last bank override

--- src/mappers/mapper034_1.rs
+++ src/mappers/mapper034_1.rs
@@ -6,11 +6,6 @@ const LAYOUT: Layout = Layout::builder()
     .prg_rom_bank_size_override(32 * KIBIBYTE)
     .prg_layout(&[
         PrgWindow::new(0x6000, 0x7FFF,  8 * KIBIBYTE, PrgBank::WORK_RAM),
-        PrgWindow::new(0x8000, 0xEFFF, 28 * KIBIBYTE, PrgBank::ROM.switchable(P0)),
-        PrgWindow::new(0xF000, 0xFFFF,  4 * KIBIBYTE, PrgBank::ROM.fixed_index(-1)),
-    ])
-    .prg_layout(&[
-        PrgWindow::new(0x6000, 0x7FFF,  8 * KIBIBYTE, PrgBank::WORK_RAM),
         PrgWindow::new(0x8000, 0xFFFF, 32 * KIBIBYTE, PrgBank::ROM.switchable(P0)),
     ])
     // Oversize definition. The actual cartridge only uses 64KiB.
