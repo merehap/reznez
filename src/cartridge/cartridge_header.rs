f75e4c01926610670f198de0cb19f75d781f8921
Sean Erle Johnson <merehap@protonmail.com>
2025-08-15 07:32:00 -0700

    Create CartridgeHeaderBuilder

--- src/cartridge/cartridge_header.rs
+++ src/cartridge/cartridge_header.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-use splitbits::{splitbits, splitbits_named};
+use splitbits::{combinebits, splitbits, splitbits_named};
 
 use crate::ppu::name_table::name_table_mirroring::NameTableMirroring;
 use crate::util::unit::KIBIBYTE;
@@ -32,7 +32,12 @@ pub struct CartridgeHeader {
 }
 
 impl CartridgeHeader {
-    pub fn parse(header: [u8; 16], data_hash: u32) -> Result<Self, String> {
+    pub fn parse(header: [u8; 16], full_hash: u32) -> Result<CartridgeHeader, String> {
+        let mut builder = CartridgeHeaderBuilder::new();
+        builder
+            .full_hash(full_hash)
+            .console_type(ConsoleType::Nes);
+
         if &header[0..4] != INES_HEADER_CONSTANT {
             return Err(format!(
                 "Cannot load non-iNES ROM. Found {:?} but need {:?}.",
@@ -41,36 +46,40 @@ impl CartridgeHeader {
             ));
         }
 
-        let prg_rom_chunk_count = header[4] as u32;
-        let chr_rom_chunk_count = header[5] as u32;
+        builder
+            .prg_rom_size(header[4] as u32 * PRG_ROM_CHUNK_LENGTH as u32)
+            .chr_rom_size(header[5] as u32 * CHR_ROM_CHUNK_LENGTH as u32);
 
-        let (lower_mapper_number, four_screen, trainer_enabled, has_persistent_memory, vertical_mirroring) =
+        let (low_mapper_number, four_screen, trainer_enabled, has_persistent_memory, vertical_mirroring) =
             splitbits_named!(header[6], "llllftpv");
-        let (upper_mapper_number, ines2, play_choice_enabled, vs_unisystem_enabled) =
-            splitbits_named!(header[7], "uuuuiipv");
-        let ines2_present = ines2 == 0b10;
+        let (mid_mapper_number, ines2, play_choice_enabled, vs_unisystem_enabled) =
+            splitbits_named!(header[7], "mmmmiipv");
 
+        builder.has_persistent_memory(has_persistent_memory);
+        let ines2_present = ines2 == 0b10;
         if trainer_enabled {
             return Err("Trainer isn't implemented yet.".to_string());
         }
 
-        let mut mapper_number = u16::from((upper_mapper_number << 4) | lower_mapper_number);
-        let mut submapper_number = None;
-        let mut prg_work = None;
-        let mut prg_save = None;
-        let mut chr_work = None;
-        let mut chr_save = None;
+        let mut high_mapper_number = 0b0000;
         if ines2_present {
-            mapper_number |= u16::from(header[8] & 0b1111) << 8;
-            submapper_number = Some(header[8] >> 4);
+            high_mapper_number = header[8] & 0b1111;
+            builder.submapper_number(header[8] >> 4);
             let prg_sizes = splitbits!(min=u32, header[10], "sssswwww");
-            prg_work = Some(if prg_sizes.w > 0 { 64 << prg_sizes.w } else { 0 });
-            prg_save = Some(if prg_sizes.s > 0 { 64 << prg_sizes.s } else { 0 });
+            let prg_work = if prg_sizes.w > 0 { 64 << prg_sizes.w } else { 0 };
+            builder.prg_work_ram_size(prg_work);
+            let prg_save = if prg_sizes.s > 0 { 64 << prg_sizes.s } else { 0 };
+            builder.prg_save_ram_size(prg_save);
 
             let chr_sizes = splitbits!(min=u32, header[11], "sssswwww");
-            chr_work = Some(if chr_sizes.w > 0 { 64 << chr_sizes.w } else { 0 });
-            chr_save = Some(if chr_sizes.s > 0 { 64 << chr_sizes.s } else { 0 });
+            let chr_work = if chr_sizes.w > 0 { 64 << chr_sizes.w } else { 0 };
+            builder.chr_work_ram_size(chr_work);
+            let chr_save = if chr_sizes.s > 0 { 64 << chr_sizes.s } else { 0 };
+            builder.chr_save_ram_size(chr_save);
         }
+
+        let mapper_number = combinebits!(high_mapper_number, mid_mapper_number, low_mapper_number, "0000uuuummmmllll");
+        builder.mapper_number(mapper_number);
 
         if play_choice_enabled {
             return Err("PlayChoice isn't implemented yet.".to_string());
@@ -80,34 +89,15 @@ impl CartridgeHeader {
             return Err("VS Unisystem isn't implemented yet.".to_string());
         }
 
-        let name_table_mirroring = if four_screen {
+        if four_screen {
             // Four screen mirroring isn't a real mirroring, the mapper will have to define what it means.
-            None
         } else if vertical_mirroring {
-            Some(NameTableMirroring::VERTICAL)
+            builder.name_table_mirroring(NameTableMirroring::VERTICAL);
         } else {
-            Some(NameTableMirroring::HORIZONTAL)
+            builder.name_table_mirroring(NameTableMirroring::HORIZONTAL);
         };
 
-        Ok(CartridgeHeader {
-            mapper_number: Some(mapper_number),
-            submapper_number,
-            name_table_mirroring,
-            has_persistent_memory: Some(has_persistent_memory),
-            console_type: Some(ConsoleType::Nes),
-
-            full_hash: Some(data_hash),
-            // Hashes have to be set afterwards since they depend on the data, which isn't in the header proper.
-            prg_rom_hash: None,
-
-            prg_rom_size: Some(prg_rom_chunk_count * PRG_ROM_CHUNK_LENGTH as u32),
-            prg_work_ram_size: prg_work,
-            prg_save_ram_size: prg_save,
-
-            chr_rom_size: Some(chr_rom_chunk_count * CHR_ROM_CHUNK_LENGTH as u32),
-            chr_work_ram_size: chr_work,
-            chr_save_ram_size: chr_save,
-        })
+        Ok(builder.build())
     }
 
     pub fn mapper_number(&self) -> Option<u16> {
@@ -169,6 +159,133 @@ impl CartridgeHeader {
 
     pub fn set_prg_rom_hash(&mut self, prg_rom_hash: u32) {
         self.prg_rom_hash = Some(prg_rom_hash);
+    }
+}
+
+pub struct CartridgeHeaderBuilder {
+    mapper_number: Option<u16>,
+    submapper_number: Option<u8>,
+
+    name_table_mirroring: Option<NameTableMirroring>,
+    has_persistent_memory: Option<bool>,
+    console_type: Option<ConsoleType>,
+
+    full_hash: Option<u32>,
+    prg_rom_hash: Option<u32>,
+
+    prg_rom_size: Option<u32>,
+    prg_work_ram_size: Option<u32>,
+    prg_save_ram_size: Option<u32>,
+
+    chr_rom_size: Option<u32>,
+    chr_work_ram_size: Option<u32>,
+    chr_save_ram_size: Option<u32>,
+}
+
+impl CartridgeHeaderBuilder {
+    pub fn new() -> Self {
+        Self {
+            mapper_number: None,
+            submapper_number: None,
+
+            name_table_mirroring: None,
+            has_persistent_memory: None,
+            console_type: None,
+
+            full_hash: None,
+            prg_rom_hash: None,
+
+            prg_rom_size: None,
+            prg_work_ram_size: None,
+            prg_save_ram_size: None,
+
+            chr_rom_size: None,
+            chr_work_ram_size: None,
+            chr_save_ram_size: None,
+        }
+    }
+
+    pub fn mapper_number(&mut self, mapper_number: u16) -> &mut Self {
+        self.mapper_number = Some(mapper_number);
+        self
+    }
+
+    pub fn submapper_number(&mut self, submapper_number: u8) -> &mut Self {
+        self.submapper_number = Some(submapper_number);
+        self
+    }
+
+    pub fn name_table_mirroring(&mut self, name_table_mirroring: NameTableMirroring) -> &mut Self {
+        self.name_table_mirroring = Some(name_table_mirroring);
+        self
+    }
+
+    pub fn has_persistent_memory(&mut self, has_persistent_memory: bool) -> &mut Self {
+        self.has_persistent_memory = Some(has_persistent_memory);
+        self
+    }
+
+    pub fn console_type(&mut self, console_type: ConsoleType) -> &mut Self {
+        self.console_type = Some(console_type);
+        self
+    }
+
+    pub fn full_hash(&mut self, full_hash: u32) -> &mut Self {
+        self.full_hash = Some(full_hash);
+        self
+    }
+
+    pub fn prg_rom_hash(&mut self, prg_rom_hash: u32) -> &mut Self {
+        self.prg_rom_hash = Some(prg_rom_hash);
+        self
+    }
+
+    pub fn prg_rom_size(&mut self, prg_rom_size: u32) -> &mut Self {
+        self.prg_rom_size = Some(prg_rom_size);
+        self
+    }
+
+    pub fn prg_work_ram_size(&mut self, prg_work_ram_size: u32) -> &mut Self {
+        self.prg_work_ram_size = Some(prg_work_ram_size);
+        self
+    }
+
+    pub fn prg_save_ram_size(&mut self, prg_save_ram_size: u32) -> &mut Self {
+        self.prg_save_ram_size = Some(prg_save_ram_size);
+        self
+    }
+
+    pub fn chr_rom_size(&mut self, chr_rom_size: u32) -> &mut Self {
+        self.chr_rom_size = Some(chr_rom_size);
+        self
+    }
+
+    pub fn chr_work_ram_size(&mut self, chr_work_ram_size: u32) -> &mut Self {
+        self.chr_work_ram_size = Some(chr_work_ram_size);
+        self
+    }
+
+    pub fn chr_save_ram_size(&mut self, chr_save_ram_size: u32) -> &mut Self {
+        self.chr_save_ram_size = Some(chr_save_ram_size);
+        self
+    }
+
+    pub fn build(self) -> CartridgeHeader {
+        CartridgeHeader {
+            mapper_number: self.mapper_number,
+            submapper_number: self.submapper_number,
+            name_table_mirroring: self.name_table_mirroring,
+            has_persistent_memory: self.has_persistent_memory,
+            console_type: self.console_type,
+            full_hash: self.full_hash,
+            prg_rom_hash: self.prg_rom_hash,
+            prg_rom_size: self.prg_rom_size,
+            prg_work_ram_size: self.prg_work_ram_size,
+            prg_save_ram_size: self.prg_save_ram_size,
+            chr_rom_size: self.chr_rom_size,
+            chr_work_ram_size: self.chr_work_ram_size,
+            chr_save_ram_size: self.chr_save_ram_size,
+        }
     }
 }
 
