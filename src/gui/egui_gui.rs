3c5bfb3a30d7f7697b71337c494860c7dbc6280d
Sean Erle Johnson <merehap@protonmail.com>
2025-09-02 11:05:35 -0700

    Add CartridgeMetadataRenderer compile-time safety

--- src/gui/egui_gui.rs
+++ src/gui/egui_gui.rs
@@ -15,6 +15,7 @@ use winit::window::Window;
 use winit::window::{WindowBuilder, WindowId};
 use winit_input_helper::WinitInputHelper;
 
+use crate::cartridge::resolved_metadata::ResolvedMetadata;
 use crate::config::Config;
 use crate::controller::joypad::{Button, ButtonStatus};
 use crate::gui::debug_screens::name_table::NameTable;
@@ -1154,7 +1155,12 @@ impl Renderer for CartridgeMetadataRenderer {
         };
 
         let resolver = nes.metadata_resolver();
-        let final_values = resolver.resolve();
+        // Explicitly spell out each field so that if a new one is added, a compile error occurs so it becomes apparent that the
+        // field needs to be added to the GUI.
+        let ResolvedMetadata { mapper_number, submapper_number, name_table_mirroring, has_persistent_memory,
+            full_hash: _, prg_rom_hash: _, prg_rom_size, prg_work_ram_size, prg_save_ram_size,
+            chr_rom_size, chr_work_ram_size, chr_save_ram_size, console_type, region_timing_mode, miscellaneous_rom_count,
+            default_expansion_device, vs } = resolver.resolve();
         let metadata_sources = [
             &resolver.hard_coded_overrides,
             &resolver.cartridge,
@@ -1188,21 +1194,21 @@ impl Renderer for CartridgeMetadataRenderer {
                     ui.end_row();
 
                     ui.label("Mapper");
-                    ui.label(final_values.mapper_number.to_string());
+                    ui.label(mapper_number.to_string());
                     for metadata in metadata_sources {
                         ui.label(metadata.mapper_number().map(|m| m.to_string()).unwrap_or("".to_owned()));
                     }
                     ui.end_row();
 
                     ui.label("Submapper");
-                    ui.label(final_values.submapper_number.map(|s| s.to_string()).unwrap_or("N/A".to_owned()));
+                    ui.label(submapper_number.map(|s| s.to_string()).unwrap_or("N/A".to_owned()));
                     for metadata in metadata_sources {
                         ui.label(metadata.submapper_number().map(|m| m.to_string()).unwrap_or("".to_owned()));
                     }
                     ui.end_row();
 
                     ui.label("Name Table Mirroring");
-                    ui.label(final_values.name_table_mirroring.unwrap().to_string());
+                    ui.label(name_table_mirroring.unwrap().to_string());
                     for metadata in metadata_sources {
                         let mut text = String::new();
                         if let Some(mirroring) = metadata.name_table_mirroring() {
@@ -1218,84 +1224,91 @@ impl Renderer for CartridgeMetadataRenderer {
                     ui.end_row();
 
                     ui.label("Has Persistent Memory");
-                    ui.label(final_values.has_persistent_memory.to_string());
+                    ui.label(has_persistent_memory.to_string());
                     for metadata in metadata_sources {
                         ui.label(metadata.has_persistent_memory().map(|m| m.to_string()).unwrap_or("".to_owned()));
                     }
                     ui.end_row();
 
                     ui.label("Console Type");
-                    ui.label(final_values.console_type.to_string());
+                    ui.label(console_type.to_string());
                     for metadata in metadata_sources {
                         ui.label(metadata.console_type().map(|m| m.to_string()).unwrap_or("".to_owned()));
                     }
                     ui.end_row();
 
                     ui.label("PRG ROM Size");
-                    ui.label(kib_string(final_values.prg_rom_size));
+                    ui.label(kib_string(prg_rom_size));
                     for metadata in metadata_sources {
                         ui.label(metadata.prg_rom_size().map(kib_string).unwrap_or("".to_owned()));
                     }
                     ui.end_row();
 
                     ui.label("PRG Work RAM Size");
-                    ui.label(kib_string(final_values.prg_work_ram_size));
+                    ui.label(kib_string(prg_work_ram_size));
                     for metadata in metadata_sources {
                         ui.label(metadata.prg_work_ram_size().map(kib_string).unwrap_or("".to_owned()));
                     }
                     ui.end_row();
 
                     ui.label("PRG Save RAM Size");
-                    ui.label(kib_string(final_values.prg_save_ram_size));
+                    ui.label(kib_string(prg_save_ram_size));
                     for metadata in metadata_sources {
                         ui.label(metadata.prg_save_ram_size().map(kib_string).unwrap_or("".to_owned()));
                     }
                     ui.end_row();
 
                     ui.label("CHR ROM Size");
-                    ui.label(kib_string(final_values.chr_rom_size));
+                    ui.label(kib_string(chr_rom_size));
                     for metadata in metadata_sources {
                         ui.label(metadata.chr_rom_size().map(kib_string).unwrap_or("".to_owned()));
                     }
                     ui.end_row();
 
                     ui.label("CHR Work RAM Size");
-                    ui.label(kib_string(final_values.chr_work_ram_size));
+                    ui.label(kib_string(chr_work_ram_size));
                     for metadata in metadata_sources {
                         ui.label(metadata.chr_work_ram_size().map(kib_string).unwrap_or("".to_owned()));
                     }
                     ui.end_row();
 
                     ui.label("CHR Save RAM Size");
-                    ui.label(kib_string(final_values.chr_save_ram_size));
+                    ui.label(kib_string(chr_save_ram_size));
                     for metadata in metadata_sources {
                         ui.label(metadata.chr_save_ram_size().map(kib_string).unwrap_or("".to_owned()));
                     }
                     ui.end_row();
 
                     ui.label("Region Timing Mode");
-                    ui.label(format!("{:?}", final_values.region_timing_mode));
+                    ui.label(format!("{:?}", region_timing_mode));
                     for metadata in metadata_sources {
                         ui.label(metadata.timing_mode().map_or("".to_owned(), |region| format!("{region:?}")));
                     }
                     ui.end_row();
 
+                    ui.label("Miscellaneous ROM Count");
+                    ui.label(miscellaneous_rom_count.to_string());
+                    for metadata in metadata_sources {
+                        ui.label(metadata.miscellaneous_rom_count().map_or("".to_owned(), |count| format!("{count:?}")));
+                    }
+                    ui.end_row();
+
                     ui.label("Default Expansion Device");
-                    ui.label(format!("{:?}", final_values.default_expansion_device));
+                    ui.label(format!("{:?}", default_expansion_device));
                     for metadata in metadata_sources {
                         ui.label(metadata.default_expansion_device().map_or("".to_owned(), |device| format!("{device:?}")));
                     }
                     ui.end_row();
 
                     ui.label("VS Hardware Type");
-                    ui.label(final_values.clone().vs.map_or("".to_owned(), |vs| format!("{:?}", vs.hardware_type)));
+                    ui.label(vs.clone().map_or("".to_owned(), |vs| format!("{:?}", vs.hardware_type)));
                     for metadata in metadata_sources {
                         ui.label(metadata.vs_hardware_type().map_or("".to_owned(), |hardware| format!("{hardware:?}")));
                     }
                     ui.end_row();
 
                     ui.label("VS PPU Type");
-                    ui.label(final_values.vs.map_or("".to_owned(), |vs| format!("{:?}", vs.ppu_type)));
+                    ui.label(vs.map_or("".to_owned(), |vs| format!("{:?}", vs.ppu_type)));
                     for metadata in metadata_sources {
                         ui.label(metadata.vs_ppu_type().map_or("".to_owned(), |ppu| format!("{ppu:?}")));
                     }
