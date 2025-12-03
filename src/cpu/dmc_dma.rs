3c2e7776b32292360697d149e362d1a468d9a65d
Sean Erle Johnson <merehap@protonmail.com>
2025-12-03 09:06:08 -0800

    Move DmcDmaState logic into DmcDma

--- src/cpu/dmc_dma.rs
+++ src/cpu/dmc_dma.rs
@@ -95,12 +95,35 @@ impl DmcDma {
     pub fn start_reload(&mut self) {
         info!(target: "apuevents", "DMC DMA Reload starting. CPU will halt soon.");
         assert_eq!(self.state, DmcDmaState::Idle);
-        self.state = DmcDmaState::TryHalt;
+        self.state = DmcDmaState::TryEnable;
         self.latest_action = DmcDmaAction::DoNothing;
     }
 
     pub fn step(&mut self, is_cpu_read_step: bool, parity: CycleParity) {
-        self.latest_action = self.state.step(is_cpu_read_step, parity);
+        use DmcDmaAction as Action;
+        use DmcDmaState as State;
+
+        let still_waiting_for_get = self.state == State::WaitingForGet && parity == CycleParity::Put;
+        let still_trying_to_halt = self.state == State::TryEnable && !is_cpu_read_step;
+        if still_waiting_for_get || still_trying_to_halt {
+            self.latest_action = Action::DoNothing;
+            return;
+        }
+
+        if self.state == State::TryRead && parity == CycleParity::Get {
+            self.latest_action = Action::Align;
+            return;
+        }
+
+        (self.latest_action, self.state) = match self.state {
+            State::Idle => (Action::DoNothing, State::Idle),
+            State::WaitingForGet => (Action::DoNothing, State::FirstSkip),
+            State::FirstSkip => (Action::DoNothing, State::SecondSkip),
+            State::SecondSkip => (Action::DoNothing, State::TryEnable),
+            State::TryEnable => (Action::Halt, State::Dummy),
+            State::Dummy => (Action::Dummy, State::TryRead),
+            State::TryRead => (Action::Read, State::Idle),
+        };
     }
 }
 
@@ -110,36 +133,9 @@ pub enum DmcDmaState {
     WaitingForGet,
     FirstSkip,
     SecondSkip,
-    TryHalt,
+    TryEnable,
     Dummy,
     TryRead,
-}
-
-impl DmcDmaState {
-    fn step(&mut self, is_cpu_read_step: bool, parity: CycleParity) -> DmcDmaAction {
-        let still_waiting_for_get = *self == Self::WaitingForGet && parity == CycleParity::Put;
-        let still_trying_to_halt = *self == Self::TryHalt && !is_cpu_read_step;
-        if still_waiting_for_get || still_trying_to_halt {
-            return DmcDmaAction::DoNothing;
-        }
-
-        if *self == Self::TryRead && parity == CycleParity::Get {
-            return DmcDmaAction::Align;
-        }
-
-        let (action, next_stage) = match *self {
-            Self::Idle => (DmcDmaAction::DoNothing, Self::Idle),
-            Self::WaitingForGet => (DmcDmaAction::DoNothing, Self::FirstSkip),
-            Self::FirstSkip => (DmcDmaAction::DoNothing, Self::SecondSkip),
-            Self::SecondSkip => (DmcDmaAction::DoNothing, Self::TryHalt),
-            Self::TryHalt => (DmcDmaAction::Halt, Self::Dummy),
-            Self::Dummy => (DmcDmaAction::Dummy, Self::TryRead),
-            Self::TryRead => (DmcDmaAction::Read, Self::Idle),
-        };
-
-        *self = next_stage;
-        action
-    }
 }
 
 #[derive(PartialEq, Eq, Clone, Copy, Debug, Default, ConstParamTy)]
