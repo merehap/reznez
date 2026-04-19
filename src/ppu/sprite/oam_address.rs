819696833cc7b483757e78a119ddf5c16fab5f24
Sean Erle Johnson <merehap@protonmail.com>
2026-04-19 07:33:05 -0700

    Remove non-unnecessary sprite_start_field_index

--- src/ppu/sprite/oam_address.rs
+++ src/ppu/sprite/oam_address.rs
@@ -1,54 +1,43 @@
 use log::info;
-use ux::u2;
 
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
-pub struct OamAddress {
-    addr: u8,
-    // Buggy sprite overflow offset.
-    sprite_start_field_index: u2,
-}
+pub struct OamAddress(u8);
 
 impl OamAddress {
     pub fn from_u8(value: u8) -> OamAddress {
         info!(target: "oamaddr", "\tSetting OamAddress to 0x{value:02X}.");
-        OamAddress {
-            addr: value,
-            // This field keeps its initial value unless a sprite overflow occurs.
-            sprite_start_field_index: u2::new(0),
-        }
+        OamAddress(value)
     }
 
     pub fn reset(&mut self) {
         info!(target: "oamaddr", "\tResetting OamAddress to 0x00.");
-        self.addr = 0;
-        self.sprite_start_field_index = u2::new(0);
+        self.0 = 0;
     }
 
     pub fn next_sprite(&mut self) -> bool {
         let end_reached;
-        (self.addr, end_reached) = self.addr.overflowing_add(4);
-        info!(target: "oamaddr", "\tAdvancing to next sprite OamAddress 0x{:02X}.", self.addr);
+        (self.0, end_reached) = self.0.overflowing_add(4);
+        info!(target: "oamaddr", "\tAdvancing to next sprite OamAddress 0x{:02X}.", self.0);
         end_reached
     }
 
     pub fn next_field(&mut self) -> bool {
         let end_reached;
-        (self.addr, end_reached) = self.addr.overflowing_add(1);
-        info!(target: "oamaddr", "\tAdvancing to next field OamAddress 0x{:02X}.", self.addr);
+        (self.0, end_reached) = self.0.overflowing_add(1);
+        info!(target: "oamaddr", "\tAdvancing to next field OamAddress 0x{:02X}.", self.0);
         end_reached
     }
 
     pub fn corrupt_sprite_y_index(&mut self) {
-        self.addr = self.addr.wrapping_add(1);
-        if self.addr % 4 == 0 {
-            self.addr -= 4;
+        self.0 = self.0.wrapping_add(1);
+        if self.0 % 4 == 0 {
+            self.0 -= 4;
         }
 
-        self.sprite_start_field_index = u2::new(self.addr % 4);
-        info!(target: "oamaddr", "\tCorrupting OamAddress 0x{:02X}.", self.to_u8());
+        info!(target: "oamaddr", "\tCorrupting OamAddress 0x{:02X}.", self.0);
     }
 
     pub fn to_u8(self) -> u8 {
-        self.addr
+        self.0
     }
 }
